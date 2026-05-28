1c77c77e021db75e1810d37ad6aac82f1405a186
Gri-ffin <gameshunter359@gmail.com>
2026-05-28 19:39:47 +0100

    add repl loop

--- src/main.c
+++ src/main.c
@@ -2,22 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
-{
+#define TRUE 1
+#define FALSE 0
+
+int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
+  while (TRUE) {
+    printf("$ ");
+    char command[1024];
+    if (fgets(command, sizeof(command), stdin) != NULL) {
 
-  char command[1024];
-  if (fgets(command, sizeof(command), stdin) != NULL)
-  {
+      command[strcspn(command, "\n")] = '\0';
 
-    command[strcspn(command, "\n")] = '\0';
-
-    if (strcmp(command, "xyz") == 0)
-    {
-      printf("%s: command not found", command);
+      if (strcmp(command, "xyz") == 0) {
+        printf("%s: command not found\n", command);
+      }
     }
   }
 
