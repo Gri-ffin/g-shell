7c4aa555358e3549abf386ed581f61414a150dc7
Gri-ffin <gameshunter359@gmail.com>
2026-05-28 20:02:21 +0100

    handle type

--- src/main.c
+++ src/main.c
@@ -18,8 +18,17 @@ int main(int argc, char *argv[]) {
 
       if (strcmp(command, "exit") == 0) {
         exit(0);
-      } else if (strncmp(command, "echo", 4) == 0) {
+      } else if (strcmp(command, "echo") == 0) {
+        printf("\n");
+      } else if (strncmp(command, "echo ", 5) == 0) {
         printf("%s\n", command + 5);
+      } else if (strncmp(command, "type ", 5) == 0) {
+        if (strcmp(command + 5, "echo") == 0 ||
+            strcmp(command + 5, "exit") == 0) {
+          printf("%s is a builtin command.\n", command + 5);
+        } else {
+          printf("%s: not found\n", command + 5);
+        }
       } else {
         printf("%s: command not found\n", command);
       }
